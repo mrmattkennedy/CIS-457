e1c2af6454376410953d764edfa985163592823e
mrmattkennedy <kennemat@mail.gvsu.edu>
2018-10-14 22:17:55 -0400

    Added comments to Project 1

--- Project 1/Java/src/FTPClient.java
+++ Project 1/Java/src/FTPClient.java
@@ -13,87 +13,160 @@ import java.util.StringTokenizer;
 
 import javax.swing.JFileChooser;
 
+/**********************************************************************
+ * FTP Client class for Project 1. Waits for input from user, then
+ * sends makes a data connection. After that, command is sent to server.
+ * 
+ * @author Matt Kennedy, Colton Bates, Parker Skarzynski, Noah Verdeyen
+ *
+ *********************************************************************/
 public class FTPClient {
 
+	/******************************************************************
+	 * Everything happens in the main method in this class. Just takes
+	 * place in an infinite loop.
+	 * 
+	 * @param argv Args sent in from user from command line.
+	 * @throws Exception Anything that may go wrong here is caught in
+	 * a try-catch, but declaring streams requires a throw declaration.
+	 *****************************************************************/
 	public static void main(String argv[]) throws Exception {
+		
+		/** This is the command that is sent to the server. */
 		String sentence;
+		
+		/** Used to get a list from the server. */
 		String modifiedSentence = "";
-		boolean isOpen = true;
-		int number = 1;
-		boolean notEnd = true;
+		
+		/** Determines if the program should continue or not at points. */
 		int statusCode;
+		
+		/** Flag for the infinite loop to determine if continue or not. */
 		boolean clientgo = true;
+		
+		/** Port to open the control connection on. */
 		int port;
 
+		/** Reader to get input from the user. */
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		
+		/** Get the first input from user. Blocks until something happens. */
 		sentence = inFromUser.readLine();
+		
+		/** Splits the sentence according to spaces. */
 		StringTokenizer tokens = new StringTokenizer(sentence);
 
+		/** If the sentence starts with connection, then try to make the connection. */
 		if (sentence.startsWith("connect")) {
-			String serverName = tokens.nextToken(); // pass the connect command
-			serverName = tokens.nextToken();
-			int port1 = Integer.parseInt(tokens.nextToken());
-			System.out.println("You are connected to " + serverName);
-
-			Socket ControlSocket = new Socket(serverName, port1);
-			DataOutputStream outToServer = new DataOutputStream(ControlSocket.getOutputStream());
-			DataInputStream inFromServer = new DataInputStream(new BufferedInputStream(ControlSocket.getInputStream()));
-
+			//Create the variables here due to scope.
+			//This way finally block can close later if exception caught.
+			Socket ControlSocket = null;
+			DataOutputStream outToServer = null;
+			DataInputStream inFromServer = null;
+			
+			//Put everything in try block to catch exceptions.
 			try {
-				while (isOpen && clientgo) {
+				String serverName = tokens.nextToken(); // pass the connect command
+				serverName = tokens.nextToken();
+				int port1 = Integer.parseInt(tokens.nextToken());
+				
+				//Create the Control Connection and the input/output streams for commands.
+				ControlSocket = new Socket(serverName, port1);
+				outToServer = new DataOutputStream(ControlSocket.getOutputStream());
+				inFromServer = new DataInputStream(new BufferedInputStream(ControlSocket.getInputStream()));
+				System.out.println("You are connected to " + serverName);
+				
+				//Infinite loop to work in.
+				while (clientgo) {
+					System.out.println("\nWhat would you like to do next: "
+							+ "\n list: || retr: file.txt || stor: || quit:\n\n");
+					//Get the first input from user. Blocks until something happens.
 					sentence = inFromUser.readLine();
 					modifiedSentence = "";
-	
+					
+					//List command
 					if (sentence.equals("list:")) {
-	
+						//Set port equal to port + 2, as per project guidelines.
 						port = port1 + 2;
+						
+						//Send the command to the server.
 						outToServer.writeBytes(port + " " + sentence + " " + '\n');
 						
+						//Create a ServerSocket that blocks until a connection is made with the server.
 						ServerSocket welcomeData = new ServerSocket(port);
 						Socket dataSocket = welcomeData.accept();
+						//Input stream to receive data from server on data connection.
 						DataInputStream inData = new DataInputStream(new BufferedInputStream(dataSocket.getInputStream()));
+						
+						//Wait while data becomes available. 
+						//This command will execute too quickly without waiting.
 						while (inData.available() == 0)
 							Thread.sleep(20);
 	
-						
+						//Read data while data is available.
 						while (inData.available() > 0) 
 							modifiedSentence += inData.readUTF();
 						
+						//Print list.
 						System.out.println("List is \n" + modifiedSentence);
-	
+						
+						//Close server socket and the data socket.
 						welcomeData.close();
 						dataSocket.close();
+						
+					//Retrieve command
 					} else if (sentence.startsWith("retr:")) {
+						//Set port equal to port + 2, as per project guidelines.
 						port = port1 + 2;
+						//Send the command to the server.
 						outToServer.writeBytes(port + " " + sentence + " " + '\n');
+						//Read status code with readInt(), which blocks until all 4 bytes read.
 						statusCode = inFromServer.readInt();
+						
+						//If status code is 550 (error).
 						if (statusCode == 550) {
 							System.out.println("Did not work.");
-							System.out.println("\nWhat would you like to do next: \n list: || retr: file.txt ||stor: || quit:\n\n");
 							continue;
+							
+						//If status code is 200 (no error).
 						} else if (statusCode == 200) {
 							ServerSocket welcomeData = new ServerSocket(port);
 							Socket dataSocket = welcomeData.accept();
 							DataInputStream inData = new DataInputStream(new BufferedInputStream(dataSocket.getInputStream()));
 							
-							//readUTF was giving errors, so just parse the sentence
+							//readUTF was giving errors, so just parse the sentence to get the file name.
 							String[] temp = sentence.split(" ");
+							
+							//Not necessary but helpful - use JFileChooser to select directory.
+							//If none is chosen, default to working directory.
 							JFileChooser chooser=new JFileChooser();
 							chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 							chooser.showSaveDialog(null);
 							String filePath = chooser.getSelectedFile() + "/" + temp[temp.length - 1];
-							System.out.println(filePath);
+							if (chooser.getSelectedFile() == null)
+								filePath = System.getProperty("user.dir") + "/" + temp[temp.length - 1];
+							
+							//Create a byte array of a size given by the inData stream to get exact file size.
 							byte[] dataIn = new byte[inData.readInt()];
+							
+							//Reads bytes from the inData stream and places them in dataIn byte array.
 							inData.readFully(dataIn);
+							
+							//Use FileOutputStream to write byes to new file.
 							try (FileOutputStream fos = new FileOutputStream(filePath)) {
 								   fos.write(dataIn);
 							}
+							
+							//Close everything.
 							inData.close();
 							welcomeData.close();
 							dataSocket.close();
 						}
 						
+					//Store command.
 					} else if (sentence.startsWith("stor:")) {
+						//Use JFileChooser to select file.
+						//Helpful because if no file selected, no command is sent, and no connection is opened.
 						JFileChooser chooser=new JFileChooser();
 						chooser.showSaveDialog(null);
 						File fileToSend = chooser.getSelectedFile();
@@ -102,22 +175,35 @@ public class FTPClient {
 							System.out.println("\nWhat would you like to do next: \n list: || retr: file.txt ||stor: || quit:\n\n");
 							continue;
 						}
+						
+						//Create the FileInputStream to get bytes.
 						FileInputStream fis = new FileInputStream(fileToSend);
 						BufferedInputStream bis = new BufferedInputStream(fis);
-						System.out.println(fileToSend.getAbsolutePath());
+						
+						//Get port and send command to server.
 						port = port1 + 2;
 						outToServer.writeBytes(port + " " + sentence + " " + '\n');
 						
+						//Create dataSocket connection.
 						ServerSocket welcomeData = new ServerSocket(port);
 						Socket dataSocket = welcomeData.accept();
 						DataOutputStream outData = new DataOutputStream(new BufferedOutputStream(dataSocket.getOutputStream()));
 						
+						//Get size of file, create byte array of that size.
 						long length = fileToSend.length();
 						byte[] bytesToSend = new byte[(int) length];
+						
+						//Reads bytes in to the bytesToSend array.
 						bis.read(bytesToSend, 0, (int)length);
+						
+						//Sends the size of the array followed by the name of the file.
 						outData.writeInt((int)length);
 						outData.writeUTF(fileToSend.getName());
+						
+						//Writes byte array
 						outData.write(bytesToSend);
+						
+						//Close everything.
 						bis.close();
 						fis.close();
 						outData.close();
@@ -128,19 +214,22 @@ public class FTPClient {
 					} else if (sentence.equals("quit:")) {
 						outToServer.writeBytes(sentence + " " + '\n');
 						clientgo = false;
+						
 					} else {
 						System.out.println("Command not recognized.");
 					}
-					
-					if (!sentence.equals("quit:"))
-							System.out.println("\nWhat would you like to do next: \n list: || retr: file.txt ||stor: || quit:\n\n");
 				}
 			} catch (Exception e) {
 				System.out.println("Error: " + e.toString());
 			} finally {
-				ControlSocket.close();	
+				try  {
+					ControlSocket.close();
+					outToServer.close();
+					inFromServer.close();
+				} catch (Exception e) {
+					return;
+				}
 			}
-			ControlSocket.close();	
 		}
 	}
 }
--- Project 1/Java/src/FTPServerThread.java
+++ Project 1/Java/src/FTPServerThread.java
@@ -13,17 +13,48 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
+/**********************************************************************
+ * FTP ServerThread class for Project 1. This is where the server
+ * processes commands for the client. The controlSocket is already
+ * created and sent in to the constructor of the class. No static
+ * variables because everything is out of the run() method.
+ * 
+ * @author Matt Kennedy, Colton Bates, Parker Skarzynski, Noah Verdeyen
+ *
+ *********************************************************************/
 public class FTPServerThread extends Thread {
-	private Socket controlSocket;
-	private DataOutputStream outToClient;
-	private BufferedReader inFromClient;
-	private List<String> clientTokens;
-	private String clientCommand;
-	private boolean clientGo;
 	
+	/** controlSocket that reads and sends commands to client. */
+	private Socket controlSocket;
+	
+	/** Stream to write commands and info to client. */
+	private DataOutputStream outToClient;
+	
+	/** Stream to read commands and info from client. */
+	private BufferedReader inFromClient;
+	
+	/** List to hold tokens from the client.
+	 * The size varies, so a list is easier to use than a String object. */
+	private List<String> clientTokens;
+	
+	/** The command the client sent to the server. */
+	private String clientCommand;
+	
+	/** Determines if the infinite loop should continue. */
+	private boolean serverGo;
+	
+	
+	/******************************************************************
+	 * Constructor for the serverThread. Initializes the streams using
+	 * the controlSocket sent in.
+	 * 
+	 * @param controlSocket Control Socket for the client/server 
+	 * connection.
+	 * @param port The port the server will run on.
+	 *****************************************************************/
 	public FTPServerThread(Socket controlSocket, int port)
     {
-		clientGo = true;
+		serverGo = true;
 		this.controlSocket = controlSocket;
         clientTokens = new ArrayList<String>();
 		try {
@@ -34,16 +65,22 @@ public class FTPServerThread extends Thread {
 		}
     }
 	
-	//Might be deprecated? Not sure, it automatically overrides without this,
+	//Override might be deprecated? Not sure, it automatically overrides without this,
 	//but helps readability to show this overrides the super run() method.
 	@Override
 	public void run() {
 		try {
-			while (clientGo) {
+			while (serverGo) {
+				//Block until a command is read from the client.
 				String fromClient = inFromClient.readLine();
+				//Clear the array list that held client commands. 
 				clientTokens.clear();
 				StringTokenizer tokens = null;
+				
+				//If the client quit, then fromClient is null sometimes.
+				//Helps prevent potential issues.
 				if (!(fromClient == null)) {
+					//Recreate tokenizer.
 					tokens = new StringTokenizer(fromClient);
 					
 					while (tokens.hasMoreTokens())
@@ -51,6 +88,8 @@ public class FTPServerThread extends Thread {
 					
 					System.out.println(clientTokens);
 					
+					//If the size > 1, the command is 1. Ex: quit: command.
+					//Used for when a port is necessary for a data connection.
 					if (clientTokens.size() > 1)
 						clientCommand = clientTokens.get(1);
 					else 
@@ -58,18 +97,27 @@ public class FTPServerThread extends Thread {
 				} else
 					clientCommand = "quit:";
 				
+				//List command.
 				if (clientCommand.equals("list:")) {
+					//Create a data socket on the given port.
 					Socket dataSocket = makeDataSocket(Integer.parseInt(clientTokens.get(0)));
 					DataOutputStream dataOutToClient = new DataOutputStream(dataSocket.getOutputStream());
+					
+					//Get all the files as a String.
 					String files = getAllFiles();
+					
+					//Use writeUTF(String) to send the file String to client.
 					dataOutToClient.writeUTF(files);
+					//Helps clear the stream before closing.
 					dataOutToClient.flush();
 					dataOutToClient.close();
 					System.out.println("List sent successfully");
 					dataSocket.close();
 					System.out.println("Data Socket closed.\n");
-	
+				
+				//Retrieve command.
 				} else if (clientCommand.equals("retr:")) {
+					//If not exactly 3 tokens, status code is 550 (error).
 					if (clientTokens.size() < 3) {
 						System.out.println("No file supplied.");
 						outToClient.writeInt(550);
@@ -77,59 +125,95 @@ public class FTPServerThread extends Thread {
 					}
 					String filePath = System.getProperty("user.dir") + "/";
 					File fileToSend = new File(filePath + clientTokens.get(2));
+					
+					//If the file doesn't exist, status code 550 (error).
 					if (!fileToSend.exists()) {
 						System.out.println("File does not exist.");
 						outToClient.writeInt(550);
 						continue;
 					} else {
+						//If all good, open dataSocket on given port.
 						outToClient.writeInt(200);
 						Socket dataSocket = makeDataSocket(Integer.parseInt(clientTokens.get(0)));
 						DataOutputStream outData = new DataOutputStream(new BufferedOutputStream(dataSocket.getOutputStream()));
+						//Use BufferedInputStream to read file in as bytes.
 						FileInputStream fis = new FileInputStream(fileToSend);
 						BufferedInputStream bis = new BufferedInputStream(fis);
 						
+						//Get size of file.
 						long length = fileToSend.length();
+						//Create byte array that is the size of the file.
 						byte[] bytesToSend = new byte[(int) length];
+						//Read bytes into byte array bytesToSend.
 						bis.read(bytesToSend, 0, (int)length);
+						//Send the size of the file to the client.
 						outData.writeInt((int)length);
+						//Send the byte array.
 						outData.write(bytesToSend);
+						//Helps clear stream before closing.
 						outData.flush();
 						outData.close();
 						bis.close();
 						fis.close();
 						dataSocket.close();
 					}
-					
+				
+				//Store command.
 				} else if (clientCommand.equals("stor:")) {
 					//Skip status code, no need to check if file exists if user is forced to choose.
 					Socket dataSocket = makeDataSocket(Integer.parseInt(clientTokens.get(0)));
 					DataInputStream inData = new DataInputStream(new BufferedInputStream(dataSocket.getInputStream()));
+					//readInt() from client to get byte array size.
 					byte[] dataIn = new byte[inData.readInt()];
 					while (inData.available() == 0)
 						Thread.sleep(20);
 					
+					//Get the server working directory.
 					String filePath = System.getProperty("user.dir") + "/";
+					//Get fhe file name from client.
 					filePath += inData.readUTF();
 					
+					//Read the bytes for the file in.
 					inData.readFully(dataIn);
+					
+					//Write bytes to file.
 					try (FileOutputStream fos = new FileOutputStream(filePath)) {
 						   fos.write(dataIn);
 					}
 					inData.close();
 					dataSocket.close();
+					
+				//Quit command.
 				} else if (clientCommand.equals("quit:")) {
-					outToClient.writeUTF("quit:");
+					System.out.println("Closing connection " + controlSocket.getInetAddress().getHostName() + ".");
 					outToClient.close();
 					inFromClient.close();
-					clientGo = false;
+					controlSocket.close();
+					serverGo = false;
+					return;
 				}
 			}
 		} catch (Exception e) {
 			System.out.println("Error: " + e.toString());
 			e.printStackTrace();
+		} finally {
+			try {
+				outToClient.close();
+				inFromClient.close();
+				controlSocket.close();
+			} catch (Exception e) {
+				return;
+			}
 		}
 	}
 	
+	/******************************************************************
+	 * Creates a data socket for the server and client to send data
+	 * over.
+	 * 
+	 * @param port The port the server will run on.
+	 * @return Socket The dataSocket object.
+	 *****************************************************************/
 	private Socket makeDataSocket(int port) {
 		System.out.println("Making data socket...");
 		try {
@@ -140,6 +224,11 @@ public class FTPServerThread extends Thread {
 		return null;
 	}
 	
+	/******************************************************************
+	 * Gets a list of all files in the working directory.
+	 * 
+	 * @return The String of all files.
+	 *****************************************************************/
 	private static String getAllFiles() {
 		File curDir = new File(".");
 		File[] files = curDir.listFiles();
