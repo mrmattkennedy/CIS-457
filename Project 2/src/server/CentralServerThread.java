e8bf8e6a516e076b7a4f53801c8b2e7455ef086f
mrmattkennedy <kennemat@mail.gvsu.edu>
2018-10-29 15:54:33 -0400

    Added table update for all clients for proj 2

---
+++ Project 2/src/server/CentralServerThread.java
@@ -0,0 +1,177 @@
+package server;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+/**********************************************************************
+ * FTP ServerThread class for Project 1. This is where the server processes
+ * commands for the client. The controlSocket is already created and sent in to
+ * the constructor of the class. No static variables because everything is out
+ * of the run() method.
+ * 
+ * @author Matt Kennedy, Colton Bates, Parker Skarzynski, Noah Verdeyen
+ *
+ *********************************************************************/
+public class CentralServerThread extends Thread {
+
+	/** controlSocket that reads and sends commands to client. */
+	private Socket controlSocket;
+
+	/** Stream to write commands and info to client. */
+	protected DataOutputStream outToClient;
+
+	/** Stream to read commands and info from client. */
+	private BufferedReader inFromClient;
+
+	/**
+	 * List to hold tokens from the client. The size varies, so a list is easier to
+	 * use than a String object.
+	 */
+	private List<String> clientTokens;
+
+	/** The command the client sent to the server. */
+	private String clientCommand;
+
+	/** Determines if the infinite loop should continue. */
+	private boolean serverGo;
+	
+	private String xmlFile = "null";
+
+	private static Vector<CentralServerThread> clients = new Vector<CentralServerThread>();
+	
+
+	/******************************************************************
+	 * Constructor for the serverThread. Initializes the streams using the
+	 * controlSocket sent in.
+	 * 
+	 * @param controlSocket Control Socket for the client/server connection.
+	 * @param port          The port the server will run on.
+	 *****************************************************************/
+	public CentralServerThread(Socket controlSocket, int port) {
+		serverGo = true;
+		this.controlSocket = controlSocket;
+		clientTokens = new ArrayList<String>();
+		clients.add(this);
+		try {
+			outToClient = new DataOutputStream(controlSocket.getOutputStream());
+			inFromClient = new BufferedReader(new InputStreamReader(controlSocket.getInputStream()));
+		} catch (IOException e) {
+			System.out.println("Error " + e.getLocalizedMessage());
+		}
+	}
+
+	// Override might be deprecated? Not sure, it automatically overrides without
+	// this,
+	// but helps readability to show this overrides the super run() method.
+	@Override
+	public void run() {
+		try {
+			xmlFile = inFromClient.readLine().substring(2);
+			System.out.println("xml file is " + xmlFile);
+			broadcast(xmlFile);
+			while (serverGo) {
+				// Block until a command is read from the client.
+				String fromClient = inFromClient.readLine();
+				System.out.println(fromClient);
+				// Clear the array list that held client commands.
+//				clientTokens.clear();
+//				StringTokenizer tokens = null;
+//
+//				// If the client quit, then fromClient is null sometimes.
+//				// Helps prevent potential issues.
+//				if (!(fromClient == null)) {
+//					// Recreate tokenizer.
+//					tokens = new StringTokenizer(fromClient);
+//
+//					while (tokens.hasMoreTokens())
+//						clientTokens.add(tokens.nextToken());
+//
+//					System.out.println(clientTokens);
+//
+//					// If the size > 1, the command is 1. Ex quit command.
+//					// Used for when a port is necessary for a data connection.
+//					if (clientTokens.size() > 1)
+//						clientCommand = clientTokens.get(1);
+//					else
+//						clientCommand = clientTokens.get(0);
+//				} else
+//					clientCommand = "quit";
+//
+//				// Quit command.
+//				if (clientCommand.equals("quit")) {
+//					System.out.println("Closing connection " + controlSocket.getInetAddress().getHostName() + ".");
+//					outToClient.close();
+//					inFromClient.close();
+//					controlSocket.close();
+//					serverGo = false;
+//					return;
+//				}
+			}
+		} catch (Exception e) {
+			System.out.println("Error " + e.toString());
+			e.printStackTrace();
+		} finally {
+			try {
+				outToClient.close();
+				inFromClient.close();
+				controlSocket.close();
+			} catch (Exception e) {
+				return;
+			}
+		}
+	}
+
+	/******************************************************************
+	 * Creates a data socket for the server and client to send data over.
+	 * 
+	 * @param port The port the server will run on.
+	 * @return Socket The dataSocket object.
+	 *****************************************************************/
+	private Socket makeDataSocket(int port) {
+		System.out.println("Making data socket...");
+		try {
+			return new Socket(controlSocket.getInetAddress(), port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	protected static void broadcast(String message) {
+		synchronized (clients) {
+			Enumeration<CentralServerThread> e = clients.elements();
+			
+			System.out.println("broadcasting");
+			while (e.hasMoreElements()) {
+				CentralServerThread client = e.nextElement();
+				
+				try {
+					client.outToClient.writeUTF("updateTable:");
+					client.outToClient.writeUTF(message);
+					client.outToClient.flush();
+				} catch (IOException ex) {
+					try {
+						client.join();
+					} catch (InterruptedException e1) {
+						e1.printStackTrace();
+					}
+				}
+			}
+		}
+		System.out.println("done");
+	}
+}
--- Project 2/src/user/UserClient.java
+++ Project 2/src/user/UserClient.java
@@ -1,9 +1,25 @@
+package user;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -12,9 +28,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
+import javax.swing.ListSelectionModel;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 
@@ -22,7 +40,7 @@ import javax.swing.UIManager.LookAndFeelInfo;
 //server will have a listening socket, then open 1 socket, send the file, and then close.
 //clients directly connect to user servers
 
-public class UserClient {
+public class UserClient implements ActionListener {
 	
 	private JPanel connectPanel;
 	private JPanel connectSubPanel1;
@@ -38,24 +56,29 @@ public class UserClient {
 	private JPanel searchPanel;
 	private JTextField searchField;
 	private JButton searchBtn;
+	private FileTableModel tableModel;
 	private JTable fileTable;
+	public JScrollPane pane;	
 	
 	private JPanel commandPanel;
 	private JTextField commandText;
 	private JButton commandBtn;
 	private JTextPane commandLog;
 	
-	
+	private Socket controlSocket;
+	private DataOutputStream outToServer;
+	private DataInputStream inFromServer;
 	private JFrame frame;
 	private static final int WIDTH = 650;
 	private static final int HEIGHT = 450;
+	
 	
 	public UserClient() {
 		connectPanel = new JPanel();
 		connectSubPanel1 = new JPanel();
 		connectSubPanel2 = new JPanel();
-		serverHost = new JTextField();
-		serverPort = new JTextField();
+		serverHost = new JTextField("localhost");
+		serverPort = new JTextField("11230");
 		username = new JTextField();
 		userHost= new JTextField();
 		connectBtn = new JButton("Connect");
@@ -68,6 +91,7 @@ public class UserClient {
 		userHost.setPreferredSize(new Dimension(150, 30));
 		connectBtn.setPreferredSize(new Dimension(100, 30));
 		connection.setPreferredSize(new Dimension(120, 30));
+		connectBtn.addActionListener(this);
 		
 		connectSubPanel1.add(new JLabel("Server Hostname"));
 		connectSubPanel1.add(serverHost);
@@ -88,16 +112,19 @@ public class UserClient {
 		searchPanel = new JPanel();
 		searchField = new JTextField();
 		searchBtn = new JButton("Search");
-		fileTable = new JTable();
+		tableModel = new FileTableModel();
+		fileTable = new JTable(tableModel);
+		pane = new JScrollPane(fileTable);
 		
 		searchField.setPreferredSize(new Dimension(200, 30));
 		searchBtn.setPreferredSize(new Dimension(100, 30));
-		fileTable.setPreferredSize(new Dimension(WIDTH - 100, HEIGHT / 4));
+		pane.setPreferredSize(new Dimension(WIDTH - 100, HEIGHT / 4));
+		fileTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		
 		searchPanel.add(new JLabel("Keyword"));
 		searchPanel.add(searchField);
 		searchPanel.add(searchBtn);
-		searchPanel.add(fileTable);
+		searchPanel.add(pane);
 		
 		
 		commandPanel = new JPanel();
@@ -129,7 +156,6 @@ public class UserClient {
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
 	}
 	
 	public void setLayout() {
@@ -167,6 +193,53 @@ public class UserClient {
 		frame.add(new JLabel("Search"), gbCons);
 		gbCons.gridy = 2;
 		frame.add(new JLabel("Command"), gbCons);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		Object source = arg0.getSource();
+		
+		if (source == connectBtn) {
+//			String str = "";
+//			try {
+//				str = getXMLFile();
+//			} catch (Exception e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//			int nameIndex = 0;
+//			List<String> names = new ArrayList<String>();
+//			while (true) {
+//				names.add(str.substring(str.indexOf("<name>", nameIndex ) + 6, str.indexOf("</name>", nameIndex)));
+//				nameIndex = str.indexOf("</name>", nameIndex) + 1; //sets starting point to most recent name tag
+//				if (str.indexOf("<name>", nameIndex) == -1) //checks location of next name tag
+//					break;
+//			}
+//				
+//			String speed = (String)(connection.getSelectedItem());
+//			String host = userHost.getText();
+//			String user = username.getText();
+//			for (int i = 0; i < names.size(); i++)
+//				tableModel.addVariable(speed, host, names.get(i), user);
+//			tableModel.repaintTable();
+			SocketListener sListen = new SocketListener(serverHost.getText(), 
+					serverPort.getText(),
+					(String)(connection.getSelectedItem()),
+					userHost.getText(),
+					username.getText(),
+					this);
+		}
+	}
+	
+	public void updateTable(String message) {
+		String[] newRows = message.split("[|]");
+		String[] temp;
+		for (int i = 0; i < newRows.length; i++) {
+			temp = newRows[i].split(" ");
+			for (int j = 0; j < temp.length; j++)
+				System.out.println(temp[j]);
+		}
+		//TODO: update the table.
 	}
 	
 	public static void main(String[] args) {
